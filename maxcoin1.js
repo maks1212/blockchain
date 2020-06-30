6cbcd27f6ae22adad932d1a7aa1b917130628502
it191506 <it191506@fhstp.ac.at>
2020-06-30 12:59:35 +0200

    mining the block

---
+++ maxcoin1.js
@@ -0,0 +1,75 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.info('Block is mined')
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '01/01/2020', 'Genesis Block', '0')
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let maxCoin = new Blockchain();
+console.log('mining Block1:')
+maxCoin.addBlock(new Block(1, '30/06/2020', {amount: '4444'}));
+console.log('mining Block2:')
+maxCoin.addBlock(new Block(2, '30/06/2020', {amount: '244'}));
+console.log('mining Block3:')
+maxCoin.addBlock(new Block(3, '30/06/2020', {amount: '244'}));
+
+
+console.log(JSON.stringify(maxCoin, null, 3))
+console.info('is Blockchain valid:', maxCoin.isChainValid());
+
